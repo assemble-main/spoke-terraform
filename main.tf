e2de5f75cc2ce809167d8247656f0250171e7904
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-07-02 10:21:56 -0400

    fix: pass list of strings.

--- main.tf
+++ main.tf
@@ -322,5 +322,5 @@ resource "aws_security_group_rule" "allow_public_postgres" {
   from_port = 5432
   to_port = 5432
   protocol = "tcp"
-  cidr_blocks = "0.0.0.0/0"
+  cidr_blocks = ["0.0.0.0/0"]
 }
